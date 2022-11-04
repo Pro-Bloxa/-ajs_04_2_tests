f3a642c5822523e44c56f0c4f89dffaefd2a2d27
Tania <taryutova@yandex.ru>
2022-11-04 16:57:15 +0300

    init

---
+++ src/__tests__/app.test.js
@@ -0,0 +1,12 @@
+import sortOrder from '../app';
+
+test('comparison', () => {
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  const received = sortOrder();
+  expect(received).toEqual(expected);
+});
